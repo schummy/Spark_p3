a6ea5898b1beaec0dd6424b60619848f5e5f4944
“scummy” <scummy@bigmir.net>
2016-04-06 14:50:14 +0300

    final

--- src/main/scala/SparkML.scala
+++ src/main/scala/SparkML.scala
@@ -33,7 +33,7 @@ object SparkML {
         RandomForestModel.load(sc, modelSaveDirectory)
       }
       catch {
-        case e: Exception => {
+        case e: Exception =>
           LogManager.getLogger("SparkML").warn("Can't load existing model. Will try to build new one.")
           val data = sc.textFile(inputDataDir + dataFileName)
           val parsedData: RDD[LabeledPoint] = prepareData(data)
@@ -43,7 +43,7 @@ object SparkML {
           var model = buildModel(trainingData)
           model.save(sc, modelSaveDirectory)
           model
-        }
+
       }
     }
     val inputData = sc.textFile(inputDataDir+inputDataFileName)
@@ -51,7 +51,7 @@ object SparkML {
     val scoreAndLabelsForInputData: RDD[(Double, Double)] = testModel(parsedInputData, sameModel)
     calculateMetrics(scoreAndLabelsForInputData)
    // predictProbability(parsedInputData,sameModel).foreach(println)
-    predict(parsedInputData,sameModel).foreach(println)
+    predict(parsedInputData,sameModel).foreach(a => {println (f"Probability[${a._1}%3d] = ${a._2}")})
   }
 
   def initConfig(): SparkConf = {
@@ -150,46 +150,57 @@ object SparkML {
     var featureLength: Int = activeFeaturesList.length
 
     var featurePosition = 0
-    for (i <- 0 to features.length - 1) {
-      if (activeFeaturesList.contains(i)) {
-        var value: Double = 0.0
-        if (!features(i).isEmpty) {
-          if (features(i) == "NaN") {
-            if (i == 8) value = 31.0
-            if (i == 9) value = 12.0
-            if (i == 10) value = 5.0
-            if (i == 11) value = 2.0
-            if (i == 12) value = 10.0
-            if (i == 38) value = 0.0
-            if (i == 49) value = 0.0
-          }
-          else {
-            value = features(i).toDouble
-
-            if (i == 0)
-              value = Math.round((value - 21) / 10).toDouble
-            else if (i == 14 || i == 32 || i == 34) {
-              if (value < 5000) value = 0.0
-              else if (value < 10000) value = 1.0
-              else if (value < 20000) value = 2.0
-              else if (value < 50000) value = 3.0
-              else value = 4.0
-            }
-            else if (i == 37) {
-              if (value / 12 > features(0).toDouble) value = 0.0
-              else value = math.round(value / 12).toDouble
-            }
-            else if (i == 47 || i == 48) {
-              if (value > 0) value = 1.0
-              else value = 0.0
-            }
-          }
-          featureSeq = featureSeq :+(featurePosition, value)
-        }
-        featurePosition += 1
-
+    for (i <- features.indices) {
+      if (activeFeaturesList.contains(i)){
+        val featureInfo = preprocessData(features, i, featureSeq, featurePosition)
+        featureSeq = featureInfo._1
+        featurePosition = featureInfo._2
       }
     }
     Vectors.sparse(featureLength, featureSeq)
+  }
+
+  def preprocessData(features:Array[String]
+                     , i:Int
+                     , featureSeq:Seq[(Int, Double)]
+                     , featurePosition:Int):
+  (Seq[(Int, Double)], Int) = {
+    var value: Double = 0.0
+    if (!features(i).isEmpty) {
+      if (features(i) == "NaN") {
+        if (i == 8) value = 31.0
+        if (i == 9) value = 12.0
+        if (i == 10) value = 5.0
+        if (i == 11) value = 2.0
+        if (i == 12) value = 10.0
+        if (i == 38) value = 0.0
+        if (i == 49) value = 0.0
+      }
+      else {
+        value = features(i).toDouble
+
+        if (i == 0)
+          value = Math.round((value - 21) / 10).toDouble
+        else if (i == 14 || i == 32 || i == 34) {
+          if (value < 5000) value = 0.0
+          else if (value < 10000) value = 1.0
+          else if (value < 20000) value = 2.0
+          else if (value < 50000) value = 3.0
+          else value = 4.0
+        }
+        else if (i == 37) {
+          if (value / 12 > features(0).toDouble) value = 0.0
+          else value = math.round(value / 12).toDouble
+        }
+        else if (i == 47 || i == 48) {
+          if (value > 0) value = 1.0
+          else value = 0.0
+        }
+      }
+      ( featureSeq :+(featurePosition, value),  featurePosition +1)
+    }
+    else {
+      (featureSeq, featurePosition)
+    }
   }
 }
